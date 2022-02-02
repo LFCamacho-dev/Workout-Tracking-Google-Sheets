f202843fc9ce6ed4cda9ae3077db5015ceb10e31
Luis Fernando Camacho <luisfernandoca@hotmail.com>
2022-02-02 01:21:16 -0600

    connecting to Sheety/Google SpreadSheets and adding rows of data taken from the nutritionix API

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import requests
+import datetime as dt
 
 APP_ID = os.environ.get("APP_ID")
 API_KEY = os.environ.get("API_KEY")
+NUTRI_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
 headers = {
     "x-app-id": APP_ID,
@@ -18,9 +20,36 @@ n_params = {
     "age": 40
 }
 
-response = requests.post(url="https://trackapi.nutritionix.com/v2/natural/exercise", json=n_params, headers=headers)
-response.raise_for_status()
-data = response.json()
-print(data)
+nutri_response = requests.post(url=NUTRI_ENDPOINT, json=n_params, headers=headers)
+nutri_response.raise_for_status()
+nutri_data = nutri_response.json()
+print(nutri_data)
 
+workouts = nutri_data['exercises']
 
+today_date = dt.datetime.now().strftime("%d/%m/%Y")
+now_time = dt.datetime.now().strftime("%H:%M:%S")
+# now_time = dt.datetime.now().strftime("%X")
+
+SHEETY_ENDPOINT = "https://api.sheety.co/bf637b0f90dd7856fb44c4190b0e1659/workoutTracking/workouts"
+
+sheety_headers = {"content-type": "application/json"}
+
+for exercise in workouts:
+    sheety_params = {
+        "workout": {
+            "date": f"{today_date}",
+            "time": f"{now_time}",
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheety_post_response = requests.post(url=SHEETY_ENDPOINT, json=sheety_params, headers=sheety_headers)
+    print(sheety_post_response.text)
+
+    sheety_response = requests.get(url=SHEETY_ENDPOINT)
+    sheety_response.raise_for_status()
+    sheety_data = sheety_response.text
+    print(sheety_data)
